0d2ee8b9fcb265d66aefbe4718d9e0fc4a7d3b17
wolterbwamale <benplaan1@gmail.com>
2025-11-02 13:14:25 +0300

    feat: add About Us page

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className="max-w-3xl mx-auto py-20 px-6">
+      <h1 className="text-4xl font-bold mb-6">About Us</h1>
+    </div>
+  );
+}
