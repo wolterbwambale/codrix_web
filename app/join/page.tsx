2233e46887ff27a945efcfaa498537e4bff40095
wolterbwamale <benplaan1@gmail.com>
2025-11-02 13:18:06 +0300

    feat(join): add Join the Team page

---
+++ app/join/page.tsx
@@ -0,0 +1,10 @@
+
+export default function JoinPage() {
+  return (
+    <div className="max-w-3xl mx-auto py-20 px-6 text-center">
+      <h1>
+        Join Our Team - We're Hiring Talented Individuals to Shape the Future of Digital Experiences!
+      </h1>
+    </div>
+  )
+}
