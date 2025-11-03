48c546b64d0e8406b2b938681f64188a47bd2efa
wolterbwamale <benplaan1@gmail.com>
2025-11-03 11:29:44 +0300

    feat(hero):add style on the paragraph

--- app/page.tsx
+++ app/page.tsx
@@ -24,9 +24,16 @@ export default function HomePage() {
         <h1 className="text-5xl md:text-6xl font-extrabold text-white mb-4">
           Welcome to <span className="text-blue-500">Codrix Solutions</span>
         </h1>
-        <p className="text-gray-300 text-5xl md:text-4xl font-bold max-w-2xl mx-auto mb-8">
-          Dream Big. Grow Great. Start building your website today!     
-        </p>
+        <motion.p
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+          className="text-4xl md:text-3xl font-bold max-w-3xl mx-auto mb-8 text-blue-400 drop-shadow-[0_0_15px_rgba(37,99,235,0.6)]"
+        >
+          Dream Big. Grow Great. <br />
+          Start building your website today!
+        </motion.p>
+
         <a
           href="#services"
           className="bg-blue-600 text-white px-8 py-3 rounded-lg text-lg hover:bg-blue-700 transition"
